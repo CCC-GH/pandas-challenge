1f9e380638c074f89de6c5785a02779c596219ce
CCC-GH <70607540+CCC-GH@users.noreply.github.com>
2020-10-10 19:57:36 -0500

    Oct10 update

--- PyCitySchools/PyCitySchools.py
+++ PyCitySchools/PyCitySchools.py
@@ -9,12 +9,12 @@ school_df = pd.read_csv(school_data_to_load)
 student_df = pd.read_csv(student_data_to_load)
 # Combine the data into a single dataset.  
 school_and_student_df = pd.merge(student_df, school_df, how="left", on=["school_name", "school_name"])
- # Total Schools
+# Total Schools
 numSchools = school_df['School ID'].count()
 # Total Students
 numStudents = student_df['Student ID'].count()
 # Total Budget
-totalBudget = round(school_df['budget'].sum(),2)
+totalBudget = school_df['budget'].sum()
 # Average Math Score
 avgMathScore = round(student_df['math_score'].mean(),6)
 # Average Reading Score
@@ -26,8 +26,19 @@ percPassReading = round(student_df['reading_score'][student_df['reading_score'] 
 # Percentage Overall Passing (The percentage of students that passed math **and** reading.)
 percPassBoth = round(student_df['reading_score'][(student_df['math_score'] >= 70) & (student_df['reading_score'] >= 70)].count()/numStudents*100,6)
 # Create a high level snapshot (in table form)
-summaryResults_df = {'Total Schools':[numSchools],'Total Students':[numStudents],'Total Budget':[totalBudget],'Average Math Score':[avgMathScore],'Average Reading Score':[avgReadingScore],'% Passing Math':[percPassMath],'% Passing Reading':[percPassReading],'% Overall Passing':[percPassBoth]}
-summaryResults_df
-#
-# School Summary
-#
+districtResults_df = pd.DataFrame({
+                    'Total Schools':[numSchools],
+                       'Total Students':[numStudents],
+                       'Total Budget':[totalBudget],
+                       'Average Math Score':[avgMathScore],
+                       'Average Reading Score':[avgReadingScore],
+                       '% Passing Math':[percPassMath],
+                       '% Passing Reading':[percPassReading],
+                       '% Overall Passing':[percPassBoth]},
+                      columns=['Total Schools','Total Students','Total Budget','Average Math Score','Average Reading Score','% Passing Math','% Passing Reading','% Overall Passing'])                       
+# Format Total Students with commas and Budget as money
+districtResults_df['Total Students'] = districtResults_df['Total Students'].astype(int).map('{:,}'.format)
+districtResults_df['Total Budget'] = districtResults_df['Total Budget'].astype(float).map('${:,.2f}'.format)
+districtResults_df    
+
+
